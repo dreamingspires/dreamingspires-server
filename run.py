245c55b538e4926cb9017c65ffa3f434e75f8cea
Edd Salkield <edd@salkield.uk>
2020-10-05 22:04:24 +0100

    Move run2 to run

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
-from app import app
+from app import app, socketio
 
 def main():
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    socketio.run(app, host='0.0.0.0', port=8080, debug=True)
+
+if __name__ == '__main__':
+    main()
